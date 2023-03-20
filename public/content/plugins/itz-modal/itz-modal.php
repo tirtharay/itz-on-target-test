c6228b7d55a3022257cd642e34592d7775efe5de
[tirtharay] <TRay@contractor.nexstardigital.com>
2023-03-20 14:45:53 -0700

    basic plugin created

---
+++ public/content/plugins/itz-modal/itz-modal.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ *
+ * @link              https://google.com
+ * @since             1.0.0
+ * @package           Itz_Modal
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Newsletter Opt-In Modal
+ * Plugin URI:        https://google.com
+ * Description:       Newsletter opt-in Plugin
+ * Version:           1.0.0
+ * Author:            Tirtha Ray
+ * Author URI:        https://google.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       itz-modal
+ * Domain Path:       /languages
+ */
