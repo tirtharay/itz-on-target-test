42325c4133f726b5e146fc37c42dfaa8ee33eb64
[tirtharay] <TRay@contractor.nexstardigital.com>
2023-03-21 03:17:00 -0700

    css complete

--- public/wp/wp-content/plugins/itz-modal/templates/itz-modal.php
+++ public/wp/wp-content/plugins/itz-modal/templates/itz-modal.php
@@ -1,4 +1,4 @@
-<div classs="itz-modal-backdrop">
+<div class="itz-modal-backdrop">
     <div class="itz-modal-wrap">
         <div class="itz-header">
             <h3 class="itz-title">PRUNDERGROUND NEWSLETTER</h3>
@@ -6,9 +6,7 @@
         <div class="itz-body">
             <form id="itz-newsletter" method="">
                 <input class="itz-input name" type="text" name="name" placeholder="Your Name" value="" />
-                <br />
                 <input class="itz-input email" name="email" placeholder="Email Address" value="" />
-                <br />
                 <input type="submit" value="SIGNUP" />
             </form>
         </div>
